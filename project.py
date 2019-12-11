3584ac99c4be1f7b19198e9cb194dc558d24f25d
tewodrosweldebirhan <tewodrosweldebirhan>
2019-12-11 12:07:03 +0100

    CLAHE contrast enhancement

--- project.py
+++ project.py
@@ -15,3 +15,7 @@ cv2.waitKey(0)
 
 # convert to gray-scale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# CLAHE contrast enhancement
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+image_clahe = clahe.apply(gray)
