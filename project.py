515829614a316cd53ad4941c3b7b26b8cb129c76
tewodrosweldebirhan <tewodrosweldebirhan>
2019-12-11 12:07:42 +0100

    black hair removal

--- project.py
+++ project.py
@@ -19,3 +19,13 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # CLAHE contrast enhancement
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 image_clahe = clahe.apply(gray)
+
+# black hair removal
+rectKernelBlack = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
+blackhat = cv2.morphologyEx(image_clahe, cv2.MORPH_BLACKHAT, rectKernelBlack)
+(T, threshBlack) = cv2.threshold(blackhat, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+inpaintedBlack = cv2.inpaint(image, threshBlack, 3, cv2.INPAINT_TELEA)
+
+cv2.imshow("noHair", inpaintedBlack)
+cv2.waitKey(0)
+cv2.imwrite("out/{}/hairRemoval.jpg".format(group), inpaintedBlack)
